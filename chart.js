523239d108aa4a1c5c8536d46987f644e5c117d1
SurajS2000 <suryasuraj437@gmail.com>
2024-01-11 20:17:38 +0530

    improved version

--- chart.js
+++ chart.js
@@ -1,20 +1,29 @@
 const ctx = document.getElementById('myChart');
+const range=document.querySelectorAll('input');
+const emp=document.getElementById('emp');
+const ret=document.getElementById('ret');
+
+range[0].addEventListener('input',(e)=>{console.log(emp.innerText=e.target.value+'%')})
+range[1].addEventListener('input',(e)=>{console.log(ret.innerText=e.target.value)})
 
   new Chart(ctx, {
     type: 'bar',
     data: {
       labels: [20, 35, 40, 45, 50, 55, 60],
       datasets: [{
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor:'rgb(55,48,163)'
+        data: [30, 40, 50, 60, 80, 100, 110],
+        backgroundColor:'rgb(55,48,163)',
+        maxBarThickness: 20,
       },
       {
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor:'rgb(79,70,229)'
+        data: [20, 40, 50, 70, 80, 100, 110],
+        backgroundColor:'rgb(79,70,229)',
+        maxBarThickness: 20,
       },
       {
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor:'rgb(129,140,248)'
+        data: [30, 40, 50, 70, 80, 100, 110],
+        backgroundColor:'rgb(129,140,248)',
+        maxBarThickness: 20,
       }]
     },
     options: {
@@ -24,11 +33,22 @@ const ctx = document.getElementById('myChart');
             }
           },
       scales: {
-        x: {stacked: true,},
+        x: {stacked: true,
+        grid:{
+          display:false
+        }},
         y: {
             stacked: true,
-          beginAtZero: true
+            border:{
+              dash:[5]
+            },
+          ticks:{
+            callback: function(value,index,ticks){
+              return '$'+value;
+            }
+          }
         }
       },
     }
   });
+  
